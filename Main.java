54a91e8d3f9f5de1fd413f52c62c3a57c6cb5913
Leslie Savell <leslie.savell@arkansas.gov>
2020-07-27 15:30:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+class Main {
+public static void main(String[] args) {
+  //Assign the numbers 77 and 14 to appropriate variables.
+  int dividend = 77;
+  int divisor = 14; 
+  //Divide the two variables (larger by smaller) and output the result.
+  System.out.println(dividend/divisor);
+  //Divide the two variables (larger by smaller) to get the remainder
+  //of 7 and output it to the console. Hint: Modulus
+  System.out.println(dividend%divisor);
+  //Perform the floating point division to get the result 5.5
+  //and assign the result to a float variable.
+  //Hint: May not be able to use the two variables above.
+  float result = 77f/14f;
+  //Output the float variable to the console.
+  System.out.println(result);
+ }
+}
